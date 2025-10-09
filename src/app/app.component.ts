730d403a3f40dc9f0116cb1a37e25f53173e83a7
Thiago Santos <137359517+Thwo22@users.noreply.github.com>
2025-10-08 21:55:41 -0300

    aula 9 até a aula 10d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { SecondComponentComponent } from './components/second-component/second-c
 import { TesteConsolidacaoComponent } from './components/teste-consolidacao/teste-consolidacao.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component'; 
+import { EventosComponent } from './components/eventos/eventos.component';  
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponentComponent, SecondComponentComponent, TesteConsolidacaoComponent, ParentDataComponent, DirectivesComponent],
+  imports: [RouterOutlet, FirstComponentComponent, SecondComponentComponent, TesteConsolidacaoComponent, ParentDataComponent, DirectivesComponent, IfRenderComponent, EventosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
